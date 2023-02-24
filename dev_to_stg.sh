a9ecb02c241901d4f6d6f703a974f2bfe0fa037f
EHAT32 <enatik14@gmail.com>
2023-02-24 21:17:13 +0300

    added scripts

---
+++ dev_to_stg.sh
@@ -0,0 +1,8 @@
+VAR=$(date '+%d.%m.%Y.%H.%M.%S') #date in given format
+git checkout stg #switch to stg
+git merge dev #merge with dev
+git tag "$VAR" #tag the date
+git push origin stg
+git push origin "$VAR"
+git checkout dev #switch to dev
+git commit -m "dev merged into stg"
