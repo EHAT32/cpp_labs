37e069ddf8e614bf30af4f1eaec059d610471f96
EHAT32 <82118458+EHAT32@users.noreply.github.com>
2023-04-07 20:05:36 +0300

    Update dev_stg_prd.bat

--- dev_stg_prd.bat
+++ dev_stg_prd.bat
@@ -1,11 +1,9 @@
 git switch dev
-git merge --commit main 
-git push
 git switch stg
 git merge --commit dev 
 git push
 git switch prd
 git merge --commit stg 
 git push
-git switch main
+git switch dev
 @pause
