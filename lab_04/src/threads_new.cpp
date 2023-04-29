93b01a82fd075fd61e008248a5fdcd015c0f2cbe
EHAT32 <enatik14@gmail.com>
2023-04-29 10:15:38 +0300

    added threads_new

---
+++ lab_04/src/threads_new.cpp
@@ -0,0 +1,65 @@
+ #include <iostream> //standart
+#include <cmath> //for pow()
+#include <time.h>
+#include <stdlib.h> // random number from _start to _end
+#include <thread>
+#include <chrono>
+#include <mutex>
+#include <queue>
+
+using namespace std;
+
+float formula_1(const float& x){
+    return std::pow(x, 2) - std::pow(x, 2) + x * 4 - x * 5 + x + x;
+}
+
+float formula_2(const float& x){
+    return x + x;
+}
+
+
+int main()
+{
+float x = 1;
+int n = 100000;
+float calc_1[n];
+float calc_2[n];
+std::mutex m;
+clock_t start = clock();
+
+
+thread th1([&calc_1, &x, &n, &m]()
+{   
+    for(int i = 0; i<n; i++)
+    {
+        float y = pow(x, 2) - pow(x, 2) + pow(x, 4) - pow(x, 5) + x + x;
+        m.lock();
+        calc_1[i] = y;
+        m.unlock();
+    }
+});
+
+thread th([&calc_2, &x, &n, &m]()
+{   
+    for(int i = 0; i < n; i++)
+    {
+        float y = x + x;
+        m.lock();
+        calc_2[i] = y;
+        m.unlock();
+    }
+});
+
+ [[maybe_unused]] float calc_3; 
+
+for(int i = 0; i<n; i++)
+{
+    calc_3 = calc_1[i] + calc_2[i] - calc_1[i];
+}
+th1.join();
+th.join();
+clock_t end = clock(); // time of end in flops
+double seconds = (double)(end - start) / CLOCKS_PER_SEC;
+printf("For N = %i the elapsed time is %.2e seconds\n", n, seconds);
+return 0;
+}
